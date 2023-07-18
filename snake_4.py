8142e16fefdd4ec777b840fcef711b01efd2934c
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-07-18 10:32:14 +0700

    Update: prepare to merge

--- snake_4.py
+++ snake_4.py
@@ -180,13 +180,3 @@ def main():
 
 
 main()
-
-# term = Terminal()
-
-# print(term.home + term.clear + term.move_y(term.height // 2))
-# print(term.black_on_darkkhaki(term.center('press any key to continue.')))
-
-# with term.cbreak(), term.hidden_cursor():
-#     inp = term.inkey()
-
-# print(term.move_down(2) + 'You pressed ' + term.bold(repr(inp)))
